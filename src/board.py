9dc9dc0e3faff3ce96ca4741a47fc694bb298326
Ansh Kumar <115077985+ansh3108@users.noreply.github.com>
2024-07-26 13:29:31 +0530

    Create board.py

---
+++ src/board.py
@@ -0,0 +1,82 @@
+from const import *
+from square import Square
+from piece import *
+from move import Move
+from sound import Sound
+import copy
+import os
+
+class Board:
+
+    def __init__(self):
+        self.squares = [[0, 0, 0, 0, 0, 0, 0, 0] for col in range(COLS)]
+        self.last_move = None
+        self._create()
+        self._add_pieces('white')
+        self._add_pieces('black')
+
+    def move(self, piece, move, testing=False):
+        initial = move.initial
+        final = move.final
+
+        en_passant_empty = self.squares[final.row][final.col].isempty()
+        self.squares[initial.row][initial.col].piece = None
+        self.squares[final.row][final.col].piece = piece
+
+        if isinstance(piece, Pawn):
+            diff = final.col - initial.col
+            if diff != 0 and en_passant_empty:
+                self.squares[initial.row][initial.col + diff].piece = None
+                self.squares[final.row][final.col].piece = piece
+                if not testing:
+                    sound = Sound(os.path.join('assets/sounds/capture.wav'))
+                    sound.play()
+            else:
+                self.check_promotion(piece, final)
+
+        if isinstance(piece, King):
+            if self.castling(initial, final) and not testing:
+                diff = final.col - initial.col
+                rook = piece.left_rook if (diff < 0) else piece.right_rook
+                self.move(rook, rook.moves[-1])
+
+        piece.moved = True
+        piece.clear_moves()
+        self.last_move = move
+
+    def valid_move(self, piece, move):
+        return move in piece.moves
+
+    def check_promotion(self, piece, final):
+        if final.row == 0 or final.row == 7:
+            self.squares[final.row][final.col].piece = Queen(piece.color)
+
+    def castling(self, initial, final):
+        return abs(initial.col - final.col) == 2
+
+    def set_true_en_passant(self, piece):
+        if not isinstance(piece, Pawn):
+            return
+
+        for row in range(ROWS):
+            for col in range(COLS):
+                if isinstance(self.squares[row][col].piece, Pawn):
+                    self.squares[row][col].piece.en_passant = False
+
+        piece.en_passant = True
+
+    def in_check(self, piece, move):
+        temp_piece = copy.deepcopy(piece)
+        temp_board = copy.deepcopy(self)
+        temp_board.move(temp_piece, move, testing=True)
+
+        for row in range(ROWS):
+            for col in range(COLS):
+                if temp_board.squares[row][col].has_enemy_piece(piece.color):
+                    p = temp_board.squares[row][col].piece
+                    temp_board.calc_moves(p, row, col, bool=False)
+                    for m in p.moves:
+                        if isinstance(m.final.piece, King):
+                            return True
+
+        return False
